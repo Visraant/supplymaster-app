9337e98e7bb2faa0c202327ed14080cafa761b55
Visraant <visraant@gmail.com>
2016-03-10 20:59:19 -0600

    added edit, delete, back to home page options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,46 @@ class ProductsController < ApplicationController
     render "index.html.erb"
   end
 
-  def show
-    @product_id = params[:id]
-    @product = Product.find_by(id: @product_id)
+  def new
   end
 
-  # def threedprinter
-  #   @threedprinter = Product.find_by(id: 2)
-  #   render "threedprinter.html.erb"
-  # end
+  def create
+    Product.create(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description],
+      stock_status: params[:stock_status],
+      delivery_time: params[:delivery_time]
+    )
+  end
 
-  # def pvc
-  #   @pvc = Product.find_by(id: 3)
-  #   render "pvc.html.erb"
-  # end
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description],
+      stock_status: params[:stock_status],
+      delivery_time: params[:delivery_time]
+    )
+  end
+
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+  end
 end
