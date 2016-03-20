f5e6ceebf26d5b7a9faab9c645d7326f23c0e25f
Visraant <visraant@gmail.com>
2016-03-20 16:13:40 -0500

    added orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,25 @@
 class OrdersController < ApplicationController
   def create
     order = Order.create(
-      name: params[:name],
-      price: params[:price],
-      image: params[:image],
-      description: params[:description],
-      stock_status: params[:stock_status],
-      delivery_time: params[:delivery_time],
+      quantity: params[:quantity],
+      # price: params[:price],
+      # image: params[:image],
+      # description: params[:description],
+      # stock_status: params[:stock_status],
+      # delivery_time: params[:delivery_time],
       user_id: current_user.id
     )
     
-    flash[:success] = "Product successfully created!"
-    redirect_to "/products/#{product.id}"
+    # flash[:success] = "You bought this product!"
+    redirect_to "/orders/#{order.id}"
   end
 
   def show
-    if params[:id] == "random"
-      products = Product.all
-      @product = products.sample
-    else
-      @product = Product.find_by(id: params[:id])
-    end
+    # if params[:id] == "random"
+    #   orders = Order.all
+    #   @order = order.sample
+    # else
+      # @order = Order.find_by(id: params[:id])
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,6 @@ Rails.application.routes.draw do
   patch '/images/:id' => 'images#update'
   delete '/images/:id' => 'images#destroy'
 
-  post '/products' => 'products#create'
-  get '/products/:id' => 'products#show'
+  post '/orders' => 'orders#create'
+  get '/orders/:id' => 'orders#show'
 end
