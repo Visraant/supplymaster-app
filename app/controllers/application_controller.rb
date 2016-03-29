906ae3c16621732da1a69befceb66daaa8b12165
Visraant <visraant@gmail.com>
2016-03-28 19:50:11 -0500

    added number of items in cart counter, and used before_action on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :get_carted_products
+
+  def get_carted_products
+    @carted_products = CartedProduct.where("status LIKE ? AND user_id = ?", "In Cart", current_user.id)
+  end
 end
